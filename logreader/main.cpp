f6a46b09dd61a9d14f5f38016c7dbb7598a99554
root <root@msk6.sipuni.com>
2014-09-03 09:05:30 +0400

    add version number

--- logreader/main.cpp
+++ logreader/main.cpp
@@ -1,8 +1,9 @@
 #include "CommandServer.h"
 
-
+#define VERSION_NUMBER 0.1
 int main()
 {
+	std::cout<<"VERSION_NUMBER="<<VERSION_NUMBER<<std::endl;
 	CommandServer::Instance().StartListen();
 	return 0;
 }
